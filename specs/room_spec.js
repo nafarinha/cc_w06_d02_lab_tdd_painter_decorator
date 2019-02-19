29ba135b29a6943985d96a78e9e3d2105eccad64
Nuno Farinha <nadfarinha@gmail.com>
2019-02-19 15:39:05 +0000

    Create test to evaluate the area of a room

--- specs/room_spec.js
+++ specs/room_spec.js
@@ -6,13 +6,23 @@ describe('Room', function () {
   let room;
 
   beforeEach( function () {
-    room = new Room('100', false);
+    room = new Room(100, false);
   });
 
+
+  it('should have a 100 sq m area', function () {
+    const actual = room.area;
+    const expected = 100;
+    assert.strictEqual(actual, expected);
+  });
 
   it('should start not painted', function () {
     const actual = room.painted;
     const expected = false;
     assert.strictEqual(actual, expected);
   });
+
+
+
+//end of describe
 });
